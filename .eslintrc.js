a2aaf707dc52b60ea12dcb2fd4c765fa984e55c6
Dionisie Covaliuc <dionisie.covaliuc@gmail.com>
2023-02-09 20:50:16 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {},
+  plugins: ['react', 'simple-import-sort', 'react-hooks', 'import', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'sort-imports': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'error',
+    'import/prefer-default-export': 'off',
+  },
+  overrides: [
+    {
+      extends: ['airbnb-typescript', 'plugin:@typescript-eslint/recommended'],
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+      },
+    },
+  ],
+};
